3de2eec19fe0c7c34540c394bb6d1b81ce82e41b
Almog <68508896+AlmogJakov@users.noreply.github.com>
2020-11-09 13:40:50 +0200

    Comments added.

--- main.c
+++ main.c
@@ -6,11 +6,16 @@ int main()
     double num;
     printf("Please insert a real number: ");
     scanf("%lf", &num);
-    float result = sub(add(Exponent((int)num),Power(num,3)),2); // Input:3.45. Expected output:59.1491.
+    // Example: Input:3.45. Expected output:59.1491.
+    float result = sub(add(Exponent((int)num),Power(num,3)),2);
     printf("The value of f(𝑥)=𝑒^𝑥+𝑥^3−2 at the point %lf is: %.4lf\n", num, result);
-    result = add(mul(num,3),mul(Power(num,2),2)); // Input:3.45. Expected output:34.155.
+
+    // Example: Input:3.45. Expected output:34.155.
+    result = add(mul(num,3),mul(Power(num,2),2));
     printf("The value of f(𝑥)=3x+2X^2 at the point %lf is: %.4lf\n", num, result);
-    result = sub(div(mul(Power(num,3),4),5),mul(num,2)); // Input:3.45. Expected output:25.9509.
+
+    // Example: Input:3.45. Expected output:25.9509.
+    result = sub(div(mul(Power(num,3),4),5),mul(num,2));
     printf("The value of f(𝑥)=(4x^3)/5-2x at the point %lf is: %.4lf\n", num, result);
     return 0;
 }
