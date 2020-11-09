b1a37c64901111230eda884781fe984d2c23cd20
Almog <68508896+AlmogJakov@users.noreply.github.com>
2020-11-09 13:39:45 +0200

    Fixed 'Exponent' 'Power' functions for any input.

--- power.c
+++ power.c
@@ -4,7 +4,10 @@
 double Power(double x , int y)
 {
     double result = 1; // Initialize variable with neutral organ to multiply.
-    for (int i = 0; i < y; i++)
+    if (x==0) return 0;
+    else if (y==0) return 0;
+    else if (y<0) return 1/Power(x , -y);
+    else for (int i = 0; i < y; i++)
     {
         result=result * x; // Multiply the variable by x, y times.
     }
@@ -14,6 +17,9 @@ double Power(double x , int y)
 double Exponent(int x)
 {
     // Call Power func with first variable=e, second variable=x (e^x).
-    double result = Power(EXP , x); 
+    double result;
+    if (x==0) return 1;
+    else if (x>0) result = Power(EXP , x);
+    else return 1/Power(EXP , -x);
     return result;
 }
